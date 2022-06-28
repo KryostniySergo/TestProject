de98a65aba70beeebc54ac04ad76593420601db4
Sergoza <huita@example.com>
2022-06-28 17:29:55 +0400

    change logic

--- main files/utils/checkPhoto.js
+++ main files/utils/checkPhoto.js
@@ -2,5 +2,7 @@ module.exports =  function chechPhoto(img){
     if(img.mimetype === 'image/png') return false
     if(img.mimetype === 'image/jpeg') return false
 
-    return img.size >= 10485760;
+    if(img.size >= 10485760) return true
+
+    return true
 }
